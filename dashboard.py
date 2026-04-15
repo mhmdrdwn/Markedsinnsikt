f88666a1e2d8de13734ae68080f4530a3243a389
Mohamed Radwan <mohamed@uppercase.no>
2026-04-15 11:32:26 +0200

    fix: add render yaml file

--- dashboard.py
+++ dashboard.py
@@ -715,4 +715,5 @@ def clear_chat(_):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8050, debug=False)
+    port = int(os.getenv("PORT", 8050))
+    app.run(host="0.0.0.0", port=port, debug=False)
